94e5c47f000e541dd2bd33b2a2ca4cbef9c8af1d
Vítězslav Dvořák <info@vitexsoftware.cz>
2020-07-09 10:02:58 +0200

    debian package renamed to php-vitexsoftware-ease-core

--- debian/fixversion.sh
+++ debian/fixversion.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 VERSTR=`dpkg-parsechangelog --show-field Version`
 COMPOSER_VERSTR=`echo ${VERSTR}|sed 's/-/./g'`
-echo update debian/php-ease-core/usr/share/php/EaseCore/composer.json version to ${COMPOSER_VERSTR}
-sed -i -e '/\"version\"/c\    \"version\": \"'${COMPOSER_VERSTR}'",' debian/php-ease-core/usr/share/php/EaseCore/composer.json
-echo Update debian/php-ease-core/usr/share/php/EaseCore/Atom.php version to ${VERSTR}
-sed -i -e "/static public \$frameworkVersion/c\    static public \$frameworkVersion = '${VERSTR}';" debian/php-ease-core/usr/share/php/EaseCore/Atom.php
+echo update debian/php-vitexsoftware-ease-core/usr/share/php/EaseCore/composer.json version to ${COMPOSER_VERSTR}
+sed -i -e '/\"version\"/c\    \"version\": \"'${COMPOSER_VERSTR}'",' debian/php-vitexsoftware-ease-core/usr/share/php/EaseCore/composer.json
+echo Update debian/php-vitexsoftware-ease-core/usr/share/php/EaseCore/Atom.php version to ${VERSTR}
+sed -i -e "/static public \$frameworkVersion/c\    static public \$frameworkVersion = '${VERSTR}';" debian/php-vitexsoftware-ease-core/usr/share/php/EaseCore/Atom.php
 echo Update src/Ease/Atom.php version to ${VERSTR}
 sed -i -e "/static public \$frameworkVersion/c\    static public \$frameworkVersion = '${VERSTR}';" src/Ease/Atom.php
