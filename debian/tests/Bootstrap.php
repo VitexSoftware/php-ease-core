5dc1f833aae3a188854bcc54c8c0d3f64313e740
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-06-22 11:37:53 +0200

    Use focal instead of trusty

---
+++ debian/tests/Bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Zaváděcí soubor pro provádění PHPUnit testů na EaseFrameworkem.
+ *
+ * @author     Vitex <vitex@hippy.cz>
+ * @copyright  2012-2021 Vitex@hippy.cz (G)
+ */
+
+namespace Test\Ease;
+
+if (!defined('EASE_LOGGER')) {
+    define('EASE_LOGGER', 'memory');
+}
+
+define('EASE_APPNAME', 'unitTest');
+define('EASE_EMAILTO', 'info@vitexsoftware.cz');
+
+require_once '/usr/lib/php-ease-core-dev/autoload.php';
+
