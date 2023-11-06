c14520939d25e8ed8209fc34cf60bf0c8898f572
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2023-11-06 18:14:24 +0100

    do not add v prefix before banner app version

--- src/Ease/Logger/Logging.php
+++ src/Ease/Logger/Logging.php
@@ -99,7 +99,7 @@ trait Logging
 
         $this->addStatusMessage(
             trim(($prefix ? $prefix : \Ease\Shared::appName()) .
-            ' PHP v' . phpversion() . ' EasePHP Framework v' . $version . ' ' . $suffix),
+            ' PHP v' . phpversion() . ' EasePHP Framework ' . $version . ' ' . $suffix),
             'debug'
         );
     }
