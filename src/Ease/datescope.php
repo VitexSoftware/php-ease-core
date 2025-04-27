55bc0a8454a7e649c02f1015d377e9c75bab1a64
CyberVitexus <info@vitexsoftware.cz>
2025-04-27 22:21:20 +0200

    datescope trait added

---
+++ src/Ease/datescope.php
@@ -0,0 +1,119 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the EaseCore package.
+ *
+ * (c) Vítězslav Dvořák <info@vitexsoftware.cz>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Ease;
+
+/**
+ * @author Vítězslav Dvořák <info@vitexsoftware.cz>
+ */
+trait datescope
+{
+    protected \DateTime $since;
+    protected \DateTime $until;
+
+    /**
+     * Prepare processing interval.
+     *
+     * @throws \Exception
+     */
+    public function setScope(string $scope): \DatePeriod
+    {
+        switch ($scope) {
+            case 'yesterday':
+                $this->since = (new \DateTime('yesterday'))->setTime(0, 0);
+                $this->until = (new \DateTime('yesterday'))->setTime(23, 59);
+
+                break;
+            case 'current_month':
+                $this->since = new \DateTime('first day of this month');
+                $this->until = new \DateTime();
+
+                break;
+            case 'last_month':
+                $this->since = new \DateTime('first day of last month');
+                $this->until = new \DateTime('last day of last month');
+
+                break;
+            case 'last_week':
+                $this->since = new \DateTime('first day of last week');
+                $this->until = new \DateTime('last day of last week');
+
+                break;
+            case 'last_two_months':
+                $this->since = (new \DateTime('first day of last month'))->modify('-1 month');
+                $this->until = (new \DateTime('last day of last month'));
+
+                break;
+            case 'previous_month':
+                $this->since = new \DateTime('first day of -2 month');
+                $this->until = new \DateTime('last day of -2 month');
+
+                break;
+            case 'two_months_ago':
+                $this->since = new \DateTime('first day of -3 month');
+                $this->until = new \DateTime('last day of -3 month');
+
+                break;
+            case 'this_year':
+                $this->since = new \DateTime('first day of January '.date('Y'));
+                $this->until = new \DateTime('last day of December'.date('Y'));
+
+                break;
+            case 'January':  // 1
+            case 'February': // 2
+            case 'March':    // 3
+            case 'April':    // 4
+            case 'May':      // 5
+            case 'June':     // 6
+            case 'July':     // 7
+            case 'August':   // 8
+            case 'September':// 9
+            case 'October':  // 10
+            case 'November': // 11
+            case 'December': // 12
+                $this->since = new \DateTime('first day of '.$scope.' '.date('Y'));
+                $this->until = new \DateTime('last day of '.$scope.' '.date('Y'));
+
+                break;
+
+            default:
+                if (strstr($scope, '>')) {
+                    [$begin, $end] = explode('>', $scope);
+                    $this->since = new \DateTime($begin);
+                    $this->until = new \DateTime($end);
+                } else {
+                    if (preg_match('/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/', $scope)) {
+                        $this->since = new \DateTime($scope);
+                        $this->until = (new \DateTime($scope))->setTime(23, 59, 59, 999);
+
+                        break;
+                    }
+
+                    throw new \Exception('Unknown scope '.$scope);
+                }
+
+                break;
+        }
+
+        if ($scope !== 'auto' && $scope !== 'today' && $scope !== 'yesterday') {
+            $this->since = $this->since->setTime(0, 0);
+            $this->until = $this->until->setTime(23, 59, 59, 999);
+        }
+
+        $this->obtainer->since = $this->since;
+        $this->obtainer->until = $this->until;
+        $this->scope = $scope;
+
+        return new \DatePeriod($this->since, new \DateInterval('P1D'), $this->until);
+    }
+}
